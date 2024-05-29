99903e16f1c9cb01095843afcbd47d1f63d85250
Vighnesh S <vighneshaka8921@gmail.com>
2024-05-29 15:03:24 +0530

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Users from "./pages/Users";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
