1afef797a882972b686cde1617b15f1f1ed1eb92
Vighnesh S <vighneshaka8921@gmail.com>
2024-05-29 12:13:11 +0530

    Add drawer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,12 @@
 import styles from "./style.module.scss";
+import menu from "../../assets/menu.svg";
+import close from "../../assets/x.svg";
 import logo from "../../assets/logo.png";
 import SearchBox from "./SearchBox";
+import { useState } from "react";
 
 const Header = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
   return (
     <div className={styles.headerParent}>
       <div className={styles.headerImage}>
@@ -21,6 +25,44 @@ const Header = () => {
       <div className={styles.buttons}>
         <div className="button white">Sign In</div>
         <div className="button">Sign Up</div>
+      </div>
+      <div
+        className={styles.menuIcon}
+        onClick={() => {
+          setDrawerOpen(true);
+        }}
+      >
+        <img src={menu} alt="" />
+      </div>
+
+      <div
+        className={styles.menuDrawer}
+        style={{
+          left: `${drawerOpen ? 0 : 100}%`,
+        }}
+      >
+        <ul style={{ marginRight: "auto" }} className={styles.drawerMenu}>
+          <li>Home</li>
+          <li>Features</li>
+          <li>Explore creators</li>
+          <li>FAQ</li>
+        </ul>
+
+        <SearchBox />
+
+        <div className={styles.drawerButtons}>
+          <div className="button white">Sign In</div>
+          <div className="button">Sign Up</div>
+        </div>
+
+        <div
+          className={styles.close}
+          onClick={() => {
+            setDrawerOpen(false);
+          }}
+        >
+          <img src={close} alt="" />
+        </div>
       </div>
     </div>
   );
